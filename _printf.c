e7ea4c5ab6c6e64789c0807f79cedb3580fe03a0
younesapi <younes.sh1234@gmail.com>
2023-04-24 00:01:11 +0000

    creating printf function

---
+++ _printf.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+
+int _strlen(char *ptr);
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character string.
+ *
+ * Return: number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int i, j, count;
+	char *p, c;
+	va_list args;
+
+	/* Reset i */
+	i = 0;
+	count = 0;
+	(void) j;
+	va_start(args, format);
+	while (format && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 's')
+			{
+				p = va_arg(args, char *);
+				if (!p)
+					j = write(1, "(nil)", 5);
+				else
+					j = write(1, p, (_strlen(p)));
+				count = count + _strlen(p) - 2;
+				i++;
+			}
+			else if (format[i + 1] == 'c')
+			{
+				c = va_arg(args, int);
+				j = write(1, &c, 1);
+				count = count - 1;
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				c = '%';
+				j = write(1, &c, 1);
+				count = count - 1;
+				i++;
+			}
+		}
+		else
+		{
+			c = format[i];
+			j = write(1, &c, 1);
+		}
+		i++;
+	}
+	va_end(args);
+	return (count + i);
+}
+
+/**
+ * _strlen - function that count the length of a string.
+ * @ptr: pointer.
+ *
+ * Return: i.
+ */
+int _strlen(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+		;
+	return (i);
+}
